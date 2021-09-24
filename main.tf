27ee14ff80533a3c6bd906c7f6f5c3f5654891e3
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-09-24 08:41:32 -0400

    Last working test

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 locals {
   #loops through variables in service (terraform.tfvars file)
-  service_payload            = [for _, s in var.services : s]
+  service_payload = [for _, s in var.services : s]
   #Compute new cts service block, adding ACI specific information to it. This enables for_each meta argument to loop through ACI and cts data within the same resource block.
-  synthetic_payload          = [for s in local.service_payload : merge(s, { esg = format("%s-%s-svc", var.esg_prefix, s.name), match_expression = format("ip=='%s'", s.address == "" ? s.node_address : s.address) })]
+  synthetic_payload = [for s in local.service_payload : merge(s, { esg = format("%s-%s-svc", var.esg_prefix, s.name), match_expression = format("ip=='%s'", s.address == "" ? s.node_address : s.address) })]
 
 
 }
@@ -11,11 +11,11 @@ data "aci_tenant" "this" {
   name = var.tenant_name
 }
 data "aci_application_profile" "this" {
-  name = var.application_profile_name
+  name      = var.application_profile_name
   tenant_dn = data.aci_tenant.this.id
 }
 data "aci_vrf" "this" {
-  name = var.vrf_name
+  name      = var.vrf_name
   tenant_dn = data.aci_tenant.this.id
 }
 
@@ -25,12 +25,14 @@ resource "aci_endpoint_security_group" "this" {
   application_profile_dn = data.aci_application_profile.this.id
   relation_fv_rs_scope   = data.aci_vrf.this.id
   name                   = each.value
+  flood_on_encap         = "disabled"
+  prio                   = "level3"
 }
 
 resource "aci_endpoint_security_group_selector" "this" {
   #Loop through the list of compute instances and map them to the corresponding service redirection policy and associated VIP
   for_each                   = { for _, s in local.synthetic_payload : s.id => s }
-  endpoint_security_group_dn  = aci_endpoint_security_group.this[each.value.esg].id
-  match_expression             = each.value.match_expression
-  description                  = "Service instance ${each.value.id} on node ${each.value.node}"
+  endpoint_security_group_dn = aci_endpoint_security_group.this[each.value.esg].id
+  match_expression           = each.value.match_expression
+  description                = "Service instance ${each.value.id} on node ${each.value.node}"
 }
